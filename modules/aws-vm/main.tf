33b76d438ecde24b06bac7373e3b3dd812f7d6e4
vishu42 <tewatiavishal10@gmail.com>
2025-09-22 21:21:34 +0530

    Add SSH key generation and security group to aws-vm module

--- modules/aws-vm/main.tf
+++ modules/aws-vm/main.tf
@@ -14,12 +14,57 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+# generate a ssh key pair
+resource "tls_private_key" "ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "ssh_key" {
+  key_name   = "${var.vm_name}-ssh-key"
+  public_key = tls_private_key.ssh_key.public_key_openssh
+}
+
+# Write private key to ~/.ssh folder
+resource "local_file" "private_key" {
+  content         = tls_private_key.ssh_key.private_key_pem
+  filename        = pathexpand("~/.ssh/${var.vm_name}-ssh-key")
+  file_permission = "0600"
+}
+
+# Security group to allow SSH traffic
+resource "aws_security_group" "ssh_access" {
+  name_prefix = "${var.vm_name}-ssh-sg"
+  description = "Security group for SSH access to ${var.vm_name}"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "All outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.vm_name}-ssh-sg"
+  }
+}
+
 resource "aws_instance" "vm" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.micro"
+  key_name               = aws_key_pair.ssh_key.key_name
+  vpc_security_group_ids = [aws_security_group.ssh_access.id]
 
   tags = {
     Name = var.vm_name
   }
-
 }
