500bba5b5b9e4541952de596066a0ce39a46a900
vishu42 <tewatiavishal10@gmail.com>
2025-10-29 11:13:18 +0530

    save

--- tf-stacks/aws-k8s/main.tf
+++ tf-stacks/aws-k8s/main.tf
@@ -1,27 +1,45 @@
 locals {
   vms = [
-    # "aws-vm-01",
-    # "aws-vm-02",
-    # "aws-vm-03",
-  ]
-}
-
-module "aws-vm-jump-vpc" {
-  count       = 0
-  source      = "../../modules/aws-vpc"
-  vpc_name    = "aws-vm-jump-vpc"
-  vpc_cidr    = "10.1.0.0/16"
-  igw_enabled = true
-  subnets = [
-    { name = "aws-vm-jump-subnet", cidr = "10.1.1.0/24", is_public = true, ntw_enabled = true },
+    "aws-vm-01",
+    "aws-vm-02",
+    "aws-vm-03",
   ]
   tags = {
     Environment = "development"
+    managedBy   = "terraform"
   }
 }
 
+module "aws-vm-jump-vpc" {
+  count      = 1
+  source     = "../../modules/aws-vpc"
+  vpc_name   = "aws-vm-jump-vpc"
+  vpc_cidr   = "10.1.0.0/16"
+  tags       = local.tags
+  create_igw = true
+}
+
+# public subnet
+module "aws-vm-jump-subnet" {
+  count    = 1
+  source   = "../../modules/aws-subnets"
+  vpc_id   = module.aws-vm-jump-vpc[0].vpc_id
+  vpc_name = "aws-vm-jump-vpc"
+  subnets = [{
+    name          = "aws-vm-jump-subnet"
+    cidr          = "10.1.1.0/24"
+    attach_nat_gw = true
+    routes = [{
+      gateway_id             = module.aws-vm-jump-vpc[0].igw_id
+      destination_cidr_block = "0.0.0.0/0"
+    }]
+  }]
+  tags = local.tags
+}
+
+# jump vm
 module "aws-vm-jump" {
-  count                       = 0
+  count                       = 1
   source                      = "../../modules/aws-vm"
   vm_name                     = "aws-vm-jump"
   associate_public_ip_address = true
@@ -31,7 +49,7 @@ module "aws-vm-jump" {
     protocol    = "udp"
     cidr_blocks = ["0.0.0.0/0"]
   }]
-  subnet_id = module.aws-vm-jump-vpc[0].subnet_ids[0]
+  subnet_id = module.aws-vm-jump-subnet[0].subnet_ids[0]
   vpc_id    = module.aws-vm-jump-vpc[0].vpc_id
 }
 
@@ -41,42 +59,38 @@ output "aws_vm_jump_public_ip" {
   description = "Public IP address of the jump VM"
 }
 
-module "aws-k8s-vpc" {
-  count       = 0
-  source      = "../../modules/aws-vpc"
-  vpc_name    = "aws-k8s-vpc"
-  vpc_cidr    = "10.0.0.0/16"
-  igw_enabled = false
-  subnets = [
-    { name = "aws-k8s-nodes-subnet", cidr = "10.0.1.0/24" },
-  ]
-  tags = {
-    Environment = "development"
-  }
+# private subnet
+module "aws-k8s-subnet" {
+  count    = 1
+  source   = "../../modules/aws-subnets"
+  vpc_id   = module.aws-vm-jump-vpc[0].vpc_id
+  vpc_name = "aws-k8s-vpc"
+  subnets = [{
+    name = "aws-k8s-nodes-subnet"
+    cidr = "10.1.3.0/24"
+    routes = [{
+      nat_gateway_id         = module.aws-vm-jump-subnet[0].nat_gateway_map["aws-vm-jump-subnet"].id
+      destination_cidr_block = "0.0.0.0/0"
+    }]
+  }]
+  tags = local.tags
 }
-
 
 module "aws-k8s-nodes" {
   for_each  = toset(local.vms)
   source    = "../../modules/aws-vm"
   vm_name   = each.value
-  subnet_id = module.aws-k8s-vpc[0].subnet_ids[0]
-  vpc_id    = module.aws-k8s-vpc[0].vpc_id
+  subnet_id = module.aws-k8s-subnet[0].subnet_ids[0]
+  vpc_id    = module.aws-vm-jump-vpc[0].vpc_id
+  additional_ingress_security_group_rules = [{
+    description = "Kubernetes"
+    port        = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["10.1.0.0/16"]
+  }]
 }
 
-output "aws_vm_public_ip" {
-  value       = [for vm in module.aws-k8s-nodes : vm.vm_public_ip]
-  description = "Public IP addresses of the VMs"
-}
-
-# vpc peer
-module "aws-vpc-peer" {
-  count        = 0
-  source       = "../../modules/aws-vpc-peer"
-  vpc_a_id     = module.aws-vm-jump-vpc[0].vpc_id
-  vpc_b_id     = module.aws-k8s-vpc[0].vpc_id
-  subnet_a_ids = module.aws-vm-jump-vpc[0].subnet_ids
-  subnet_b_ids = module.aws-k8s-vpc[0].subnet_ids
-  vpc_a_rt_id  = module.aws-vm-jump-vpc[0].public_route_table_id
-  vpc_b_rt_id  = module.aws-k8s-vpc[0].public_route_table_id
+output "aws_vm_private_ip" {
+  value       = [for vm in module.aws-k8s-nodes : vm.vm_private_ip]
+  description = "Private IP addresses of the VMs (k8s nodes)"
 }
--- tf-stacks/aws-vpc-test/main.tf
+++ tf-stacks/aws-vpc-test/main.tf
@@ -1,48 +1,48 @@
 locals {
   tags = {
     environment = "development",
-    managedBy = "terraform"
+    managedBy   = "terraform"
   }
 }
 
 module "aws-vpc" {
-  count      = 1
+  count      = 0
   source     = "../../modules/aws-vpc"
   vpc_name   = "test-vpc"
   vpc_cidr   = "10.2.0.0/16"
   create_igw = true
-  tags = local.tags
+  tags       = local.tags
 }
 
 module "aws-public-subnet" {
-  count = 0
-  source = "../../modules/aws-subnets"
-  vpc_id = module.aws-vpc[0].vpc_id
+  count    = 0
+  source   = "../../modules/aws-subnets"
+  vpc_id   = module.aws-vpc[0].vpc_id
   vpc_name = "test-vpc"
-  subnets = [ {
-    name = "public-subnet"
-    cidr = "10.2.0.0/24"
+  subnets = [{
+    name          = "public-subnet"
+    cidr          = "10.2.0.0/24"
     attach_nat_gw = true
-    routes = [ {
-      gateway_id = module.aws-vpc[0].igw_id
+    routes = [{
+      gateway_id             = module.aws-vpc[0].igw_id
       destination_cidr_block = "0.0.0.0/0"
-    } ]
-  } ]
+    }]
+  }]
   tags = local.tags
 }
 
 module "aws-private-subnet" {
-  count = 0
-  source = "../../modules/aws-subnets"
-  vpc_id = module.aws-vpc[0].vpc_id
+  count    = 0
+  source   = "../../modules/aws-subnets"
+  vpc_id   = module.aws-vpc[0].vpc_id
   vpc_name = "test-vpc"
-  subnets = [ {
+  subnets = [{
     name = "private-subnet"
     cidr = "10.2.8.0/24"
-    routes = [ {
-      nat_gateway_id = module.aws-public-subnet[0].nat_gateway_map["public-subnet"].id
+    routes = [{
+      nat_gateway_id         = module.aws-public-subnet[0].nat_gateway_map["public-subnet"].id
       destination_cidr_block = "0.0.0.0/0"
-    } ]
-  } ]
+    }]
+  }]
   tags = local.tags
 }
